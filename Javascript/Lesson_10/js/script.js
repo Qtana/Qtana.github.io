19297a5e6241d85bac2c3e8ad163091592c1566d
Olya Pysmenna <pysmenna@mail.ru>
2016-01-22 22:26:43 +0200

    Updated js lesson 10

---
+++ Javascript/Lesson_10/js/script.js
@@ -0,0 +1,49 @@
+$(function () {
+    /* For carousel */
+
+    $('.jcarousel').jcarousel({
+        /*list: '.jcarousel-list',*/
+        animation: 'slow',
+        animation: {
+            duration: 500,
+            easing: 'linear',
+            complete: function () {
+            }
+        }
+    });
+
+    $('.jcarousel-control-prev')
+        .on('jcarouselcontrol:active', function () {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function () {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .on('jcarouselcontrol:active', function () {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function () {
+            $(this).addClass('inactive');
+        })
+        .on('click', function (e) {
+            e.preventDefault();
+        })
+        .jcarouselControl({
+            target: '+=1'
+        });
+
+    $("#country_id").selectbox();
+
+    /* For checkbox */
+    $('.niceCheck').mousedown(function () {
+        changeCheck(jQuery(this));
+    });
+    $('.niceCheck').each(function () {
+        changeCheckStart(jQuery(this));
+    });
+});
